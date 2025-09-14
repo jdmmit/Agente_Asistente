38d2d4c95f58703954cd84b53b08e13386bf0dd7
juandavidmm0718 <juandavidmm0718@gmail.com>
2025-09-14 08:56:08 +0000

    feat(env): Habilitar el servicio de Docker en la configuración de Nix
    Activa el demonio de Docker a través del archivo dev.nix para que Docker esté disponible dentro del espacio de trabajo de IDX. Esto es un requisito para construir y ejecutar la aplicación utilizando la opción de instalación con Docker.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,6 +4,9 @@
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
+  # Enable the Docker daemon
+  services.docker.enable = true;
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
